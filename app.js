05e7cfd5353c93f50f8d0ae52571e3a955f427a3
ameermaviacooder <160848651+ameermaviacooder@users.noreply.github.com>
2024-03-04 02:06:40 -0800

    Stripping Names
     45 question Stripping Names 5th assigements

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let personName = `\t\n MAVIA IBRAHIM\t\n`;
+console.log(`${personName}`);
+//stripped
+let stripped = personName.trim();
+console.log(`${stripped}`);
---
+++ app.ts
@@ -0,0 +1,8 @@
+let personName:string =`\t\n MAVIA IBRAHIM\t\n`;
+console.log(`${personName}`);
+
+//stripped
+
+let stripped:string = personName. trim();
+
+console.log(`${stripped}`);
